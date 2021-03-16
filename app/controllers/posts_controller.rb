6bfc849f8981d018c464e36ae3a131d04e618288
ayumi_miyazaki <a.miyazaki313@gmail.com>
2021-03-16 13:49:40 +0900

    編集機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,18 @@ class PostsController < ApplicationController
   end
   def create
     Post.create(post_params)
-    redirect_to new_post_path
+    redirect_to posts_path
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: '編集しました'
+    else
+      render :edit
+    end
   end
   private
   def post_params
